79d4ea7bad5a01cb206c8c81a0802d79e67554d3
Devin Jhu <devin.jhu@mths.ca>
2022-02-25 19:16:50 +0000

    added calculator for area and perimeter

---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by Devin Jhu
+# Created on February 2022
+# The area and perimeter calculator
+# Currently calculating for 3cm by 5cm rectangle
+
+
+def main():
+    # This calculates the area and perimeter
+    print("If this rectangle has the dimentions:")
+    print("3cm x 5cm")
+    print("")
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
